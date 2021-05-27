59f7b6916c6a4672bbe942c62627594a886f655f
unknown <mindydiane@gmail.com>
2021-05-27 12:11:30 -0500

    compared with code snapshot and updated js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,6 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-   //package up data as an object
-   var taskDataObj = {
-       name: taskNameInput,
-       type: taskTypeInput
-   };
-
     //check if input values are empty strings
     if (!taskNameInput || !taskTypeInput) {
         alert("You need to fill out the task form!");
@@ -19,6 +13,16 @@ var taskFormHandler = function (event) {
     }
 
     formEl.reset();
+
+    //reset form fields for next taskto be entered
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
+
+    //package up data as an object
+    var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+};
 
    //send it as an argument to createTaskEl
    createTaskEl(taskDataObj);
@@ -34,6 +38,8 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
+
+    console.dir(listItemEl);
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);  
